c476f1a0e193693b5c59a3639dd447fc9a99995f
Larry L Biehl <biehl@purdue.edu>
2020-02-12 12:54:06 -0500

    2020.02.12_updates

--- shared/SDefines.h
+++ shared/SDefines.h
@@ -1,11 +1,18 @@
-//	 									MultiSpec
+//                                     MultiSpec
 //
-//					Laboratory for Applications of Remote Sensing
-//									Purdue University
-//								West Lafayette, IN 47907
-//								 Copyright (1988-2020)
-//								(c) Purdue Research Foundation
-//									All rights reserved.
+//                   Copyright 1988-2020 Purdue Research Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at:  https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+//
+// MultiSpec is curated by the Laboratory for Applications of Remote Sensing at
+// Purdue University in West Lafayette, IN and licensed by Larry Biehl.
 //
 //	File:						SDefines.h
 //
@@ -21,7 +28,7 @@
 //	Written By:				Larry L. Biehl			Date: 03/29/1988
 //	Revised By:				Abdur Maud				Date: 06/24/2013
 //	Revised By:				Tsung Tai Yeh			Date: 09/25/2015
-//	Revised By:				Larry L. Biehl			Date: 12/02/2019
+//	Revised By:				Larry L. Biehl			Date: 01/10/2020
 //	
 //------------------------------------------------------------------------------------
 
@@ -5090,9 +5097,9 @@ typedef struct WindowInfo
 			// image overlays structure member.			
 	UInt32					numberImageOverlays;
 	
-			// The number of overlays for this window. The indices are in the 
+			// The number of vector overlays for this window. The indices are in the
 			// overlays structure member.			
-	UInt32					numberOverlays;
+	UInt32					numberVectorOverlays;
 	
 			// Size of the buffer for the offscreen pixel map.							
 			// This is needed for determining whether to unlock offscreen map		
