0a1d430429bdc695821136e7a778b9865bae7c1a
Larry L Biehl <biehl@purdue.edu>
2020-02-20 20:46:07 -0500

    rename complete

--- windows/WMultiSpec_pch.cpp
+++
@@ -1,5 +0,0 @@
-// WMultiSpec_pch.cpp : source file that includes just the standard includes
-//  WMultiSpec_pch.pch will be the pre-compiled header
-//  WMultiSpec_pch.obj will contain the pre-compiled type information
-
-#include "SMultiSpec.h"
